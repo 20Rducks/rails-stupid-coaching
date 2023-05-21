b42cac535f5d82d90c41ede10b75cf032318fbb3
robert willcox <robducks@gmail.com>
2023-05-21 03:57:35 +0100

    2nd challenge, link view, controller, url

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+  def ask
+    params[:answer]
+  end
+
+  def answer
+    @questions = params[:answer]
+
+    if @questions == 'I am going to work'
+      @answer = 'Great'
+    elsif @questions[-1] == '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
